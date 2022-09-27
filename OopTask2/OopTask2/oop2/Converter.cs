00160886f50dd958af0a83e196485df33c77c97b
tripex17 <114466222+tripex17@users.noreply.github.com>
2022-09-27 08:35:16 +0200

    Add files via upload

---
+++ OopTask2/OopTask2/oop2/Converter.cs
@@ -0,0 +1,33 @@
+﻿
+    public class Converter {
+
+        private double usd;
+        private double eur;
+        private double rub;
+
+        public Converter(double usd, double eur, double rub)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+        }
+
+        public int convertUAHToUSD(double amount)
+        {
+            return (int)Math.Round((amount / usd)); 
+        }
+
+        public int convertUSDToUAH(double amount)
+        {
+            return (int)Math.Round(amount * usd);
+        }
+
+        public int convertEURToUAH(double amount) {
+            return (int)Math.Round(amount * eur);  
+        }
+        public int convertRUBToUAH(double amount)
+        {
+            return (int)Math.Round(amount * rub); 
+        }
+    }
+
